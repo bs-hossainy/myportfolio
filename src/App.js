c5f4b4472ae469beca96335d59a2344a997ef5dd
Bismillah Hossainy <bs.hossainy@gmail.com>
2022-06-09 23:51:44 +0200

    isNavbarExpanded and setNavbarExpanded states added to expand the Navbar column-wise

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Home from "./pages/Home";
 import Experience from "./pages/Experience";
 import Projects from "./pages/Projects";
+import Blog from "./pages/Blog";
+import Books from "./pages/Books";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
@@ -19,6 +21,8 @@ function App() {
               <Route path="/" element={<Home />} />
               <Route path="/projects" element={<Projects />} />
               <Route path="/experience" element={<Experience />} />
+              <Route path="/Blog" element={<Blog />} />
+              <Route path="/Books" element={<Books />} />
           </Routes>
           <Footer />
       </Router>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
 import { Link } from "react-router-dom"
 
 import "../cssStyles/Navbar.css"
 import ReorderIcon from "@material-ui/icons/Reorder";
 
 function Navbar() {
+
+    // Clicking on the button should open the Navbar
+    const [isNavbarExpanded, setNavbarExpanded] = useState(false);
+
     return (
-        <div className="navbar">
+        <div className="navbar" id={isNavbarExpanded ? "expand" : "close"}>
+
             <div className="toggleButton">
-                <button> <ReorderIcon /> </button>
+                <button onClick={
+                    () => {
+                        setNavbarExpanded((state) => !state);
+                }}>
+                    <ReorderIcon />
+                </button>
             </div>
+
             <div className="links">
-                <Link to="/"> Home </Link>
-                <Link to="/experience"> Experience </Link>
-                <Link to="/projects"> Projects </Link>
+                <Link to="/"> HOME </Link>
+                <Link to="/experience"> EXPERIENCE </Link>
+                <Link to="/projects"> PROJECTS </Link>
+                <Link to="/Blog"> BLOG </Link>
+                <Link to="/Books"> FAV. BOOKS </Link>
             </div>
+
         </div>
     );
 }
